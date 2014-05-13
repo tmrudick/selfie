e977098ca74398a8df0bea504658f21675d74193
Tom Rudick <tmrudick@gmail.com>
2014-05-13 07:50:10 -0400

    Updating photo path

--- bin/take-photo.sh
+++ bin/take-photo.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-mkdir -p "$HOME/Documents/minute-a-day/$(date +'%Y%m%d')"
+mkdir -p "$HOME/Documents/selfies/$(date +'%Y%m%d')"
 
-imagesnap "$HOME/Documents/minute-a-day/$(date +'%Y%m%d')/photo-$(date +'%Y%m%d-%H%M').jpg" > /dev/null
-screencapture -x "$HOME/Documents/minute-a-day/$(date +'%Y%m%d')/screen-$(date +'%Y%m%d-%H%M').jpg"
+imagesnap "$HOME/Documents/selfies/$(date +'%Y%m%d')/photo-$(date +'%Y%m%d-%H%M').jpg" > /dev/null
+screencapture -x "$HOME/Documents/selfies/$(date +'%Y%m%d')/screen-$(date +'%Y%m%d-%H%M').jpg"
